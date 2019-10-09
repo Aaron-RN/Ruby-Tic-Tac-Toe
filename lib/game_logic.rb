e4ff78ca6c324e2e8fab6e2d8a740a9491cb4e15
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-10-09 14:40:15 -0400

    Added game_logic functions Added Board class which contains all information for current Game

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,5 +1,44 @@
-require './bin/main'
+# frozen_string_literal: true
 
+class BoardLogic
+  attr_reader :board, :rows, :cols, :player_turn, :turns_taken, :max_turns
+
+  def initialize(rows, cols)
+    @rows = rows
+    @cols = cols
+    @board = []
+    @player_turn = nil
+    @turns_taken = 0
+    @max_turns = rows * cols
+    for i in 1..@cols*@rows
+      @board << i
+    end
+  end
+
+  def whos_turn?(player)
+    @player_turn = player
+  end
+
+  def move_made(player, pos)
+    @board[pos - 1] = player.symbol.to_s.green if player.color == 'green'
+    @board[pos - 1] = player.symbol.to_s.red if player.color == 'red'
+    @board[pos - 1] = player.symbol.to_s.blue if player.color == 'blue'
+    @turns_taken += 1
+    game_over?
+  end
+
+  def draw?
+    puts 'DRAW!' if @turns_taken >= @max_turns
+  end
+
+  def game_over?
+    @board.each_with_index do |item, i|
+      print i, item unless @board[i].is_a? Integer
+      puts "\n"
+    end
+    draw?
+  end
+end
 class Player
   attr_reader :symbol, :positions_mark, :color
 
@@ -14,13 +53,20 @@ class Player
   end
 end
 
-def check_board_pos(board, pos_chosen)
-  if pos_chosen.Integer?
+def check_board_pos(game, pos_chosen)
+  if pos_chosen.is_a? Integer
     pos_chosen.abs
     # if
-    pos_chosen - 1 < board.length ? true : getPlayerInput
+    pos_chosen - 1 < game.board.length ? true : getPlayerInput
+  else
+    getPlayerInput
   end
-  return true if board[pos_chosen - 1].Integer?
+  return true if game.board[pos_chosen - 1].is_a? Integer
 
   false
+end
+
+def mark_position(game, player, pos)
+  player.position(pos)
+  game.move_made(player, pos)
 end
