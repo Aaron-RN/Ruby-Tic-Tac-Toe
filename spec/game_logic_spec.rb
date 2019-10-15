a4bfcb8ecf214336c2ba5cced1b13b18a25cfb3d
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-10-15 18:51:38 -0400

    Added test for checking if game won even if no more turns are available

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -18,9 +18,7 @@ RSpec.describe BoardLogic do
       game.game_over?
       expect(game.winner).to eql(player_one)
     end
-  end
 
-  describe '#draw?' do
     it 'Returns a draw if neither player has won and no more positions are available on the board' do
       game.whos_turn?(player_one)
       game.player_setup(player_one, player_two)
@@ -29,6 +27,19 @@ RSpec.describe BoardLogic do
       end
       expect(game.winner).to eql(nil)
       expect(game.game_over).to eql(true)
+    end
+
+    it 'Returns the winning player when a player has 3 matches in a row
+       and no more positions are available on the board' do
+      game.whos_turn?(player_one)
+      game.player_setup(player_one, player_two)
+      player_one.position(1)
+      player_one.position(5)
+      player_one.position(9)
+      (1..game.cols * game.rows).each do |i|
+        game.move_made(game.player_turn, i)
+      end
+      expect(game.winner).to eql(player_one)
     end
   end
 end
