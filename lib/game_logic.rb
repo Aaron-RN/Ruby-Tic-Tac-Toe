daaf3397214fee57878e8cf01701ec5b815b9ec8
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-10-15 18:41:03 -0400

    Added more Tests to RSpec and moved String color methods into game_logic.rb

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+class String
+  def red
+     "\e[31m#{self}\e[0m" 
+  end
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+  def blue
+    "\e[34m#{self}\e[0m"
+  end
+  def bold
+    "\e[1m#{self}\e[22m"
+  end
+  def underline
+    "\e[4m#{self}\e[24m"
+  end
+end
+
 class BoardLogic
   attr_reader :board, :rows, :cols, :player_turn, :player_one, :player_two,
               :turns_taken, :max_turns, :game_over, :winner
--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -8,20 +8,27 @@ RSpec.describe BoardLogic do
   let(:player_two) { Player.new('O', 'red') }
   let(:game) { BoardLogic.new(rows = 3, cols = 3) }
 
-  describe '#game.game_over?' do
+  describe '#game_over?' do
     it 'Returns the winning player when the winning combo matches the positioned marks' do
       game.whos_turn?(player_one)
       game.player_setup(player_one, player_two)
-      player_one.position([1, 2, 3])
+      player_one.position(1)
+      player_one.position(2)
+      player_one.position(3)
       game.game_over?
       expect(game.winner).to eql(player_one)
     end
   end
 
-  # describe '#whos_turn?' do
-  #   it 'Shows whose turn is next' do
-  #     expect(@player_turn).to eql(player)
-  #   end
-  # end
-
+  describe '#draw?' do
+    it 'Returns a draw if neither player has won and no more positions are available on the board' do
+      game.whos_turn?(player_one)
+      game.player_setup(player_one, player_two)
+      (1..game.cols * game.rows).each do |i|
+        game.move_made(game.player_turn, i)
+      end
+      expect(game.winner).to eql(nil)
+      expect(game.game_over).to eql(true)
+    end
+  end
 end
