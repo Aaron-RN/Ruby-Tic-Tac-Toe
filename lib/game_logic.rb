0e473b8abbd24e55e1078fa4d662d53a946e97c8
Aaron-RN <aaron.rory.newbold@gmail.com>
2019-10-16 18:20:35 -0400

    Stickler fixes

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,17 +2,21 @@
 
 class String
   def red
-     "\e[31m#{self}\e[0m"
+    "\e[31m#{self}\e[0m"
   end
+
   def green
     "\e[32m#{self}\e[0m"
   end
+
   def blue
     "\e[34m#{self}\e[0m"
   end
+
   def bold
     "\e[1m#{self}\e[22m"
   end
+
   def underline
     "\e[4m#{self}\e[24m"
   end
